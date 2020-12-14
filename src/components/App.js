2ab1512486a90a1d0a0e393107c2e5245dbc49ae
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-12-14 17:32:59 +0530

    class

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,28 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      para: false
+    };
   }
-
+  para() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
   render() {
     return (
-      <div id="main">
-        {
-          /* Do not remove this main div!! */
-
-          <button id="click" onClick={activateLasers}>
-            <p id="para">
-              Hello, I've learnt to use the full-stack evaluation tool. This
-              makes me so happy
-            </p>
+      <>
+        <div id="main">
+          {/* Do not remove this main div!! */}
+          <button id="click" onClick={() => this.setState({ para: true })}>
+            {this.state.para ? this.para() : ""}
           </button>
-        }
-      </div>
+        </div>
+      </>
     );
   }
 }
-
-export default App;
